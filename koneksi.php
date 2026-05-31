b9ae18c6b06a753e9cf0837d07faa96a6cbb1ad2
NiarRisma <niarrisma32@gmail.com>
2026-05-31 23:45:19 +0700

    update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -10,10 +10,4 @@ $conn = new mysqli(
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
-
-// Set timezone MySQL ke WIB (UTC+7)
-$conn->query("SET time_zone = '+07:00'");
-
-// Set timezone PHP ke WIB
-date_default_timezone_set('Asia/Jakarta');
 ?>
